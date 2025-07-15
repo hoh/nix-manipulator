39844f95ffbb77e962e694c4cbd0a0828edb74b1
Hugo Herter <git@hugoherter.com>
2025-07-15 11:12:59 +0200

    Update shell to use package

--- shell.nix
+++ shell.nix
@@ -1,23 +1,7 @@
-# shell.nix
-{ pkgs ? import <nixos-unstable> {} }:
+{ pkgs ? import <nixpkgs> {} }:
 
-let
-  pyEnv = pkgs.python312.withPackages (ps: [
-    ps.tree-sitter            # core bindings 0.25.x
-    ps.tree-sitter-grammars.tree-sitter-nix        # grammar wrapper → tree_sitter_nix.language()
-    ps.tree-sitter-grammars.tree-sitter-python     # grammar wrapper → tree_sitter_python.language()
-    ps.pygments
-    ps.pydantic
-
-    ps.mypy
-    ps.ruff
-    ps.pytest
-    ps.isort
-  ]);
-in pkgs.mkShell {
-  packages = [ pkgs.tree-sitter pyEnv ];
-  shellHook = ''
-    PYTHONPATH=$(pwd)
-  '';
+pkgs.mkShell {
+  buildInputs = [
+    (pkgs.callPackage ./default.nix { })
+  ];
 }
-
